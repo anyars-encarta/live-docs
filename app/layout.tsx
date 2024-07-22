ba3cec2b76235ae2a4c87e9e2a4c8f47948abca0
anyars-encarta <anyarsencarta@gmail.com>
2024-07-22 15:36:30 +0000

    Render Shad-cn button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
 
+import './globals.css';
+
 import { cn } from "@/lib/utils"
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metadata: Metadata = {
+  title: 'LiveDocks',
+  description: 'Your go-to collaborative editor'
+}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )

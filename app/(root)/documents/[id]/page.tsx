face068fa4a6e9dfae083e7e31eff5b380c8a5a1
anyars-encarta <anyarsencarta@gmail.com>
2024-07-23 11:49:11 +0000

    Implement sig-in and sign-up

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -12,7 +12,7 @@ const Document = () => {
     <div>
       <Header>
         <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'>This is a fake document title</p>
+          <p className='document-title'>Share</p>
         </div>
 
         <SignedOut>

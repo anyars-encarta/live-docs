75787c9f59591e5d826e07fa0d2333fdce0dfd57
anyars-encarta <anyarsencarta@gmail.com>
2024-07-23 09:47:34 +0000

    Add header component to editor page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { Editor } from '@/components/editor/Editor'
-import React from 'react'
+import Header from '@/components/Header'
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className='flex w-fit items-center justify-center gap-2'>
+          <p className='document-title'>This is a fake document title</p>
+        </div>
+      </Header>
+
       <Editor />
     </div>
   )

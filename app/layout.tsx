6f815eb57edce9bb5f6034af1bafedcec27f0d1c
anyars-encarta <anyarsencarta@gmail.com>
2024-07-23 10:22:03 +0000

    Install and configure clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import { Inter as FontSans } from "next/font/google"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 
 import './globals.css';
 
@@ -14,17 +21,22 @@ export const metadata: Metadata = {
   title: 'LiveDocs',
   description: 'Your go-to collaborative editor'
 }
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      baseTheme: 
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }

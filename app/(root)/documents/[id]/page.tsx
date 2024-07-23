2aae38eeb406ba9f4aad301bd8e441297aa2b824
anyars-encarta <anyarsencarta@gmail.com>
2024-07-23 09:06:47 +0000

    Add an Editor component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,11 @@
+import { Editor } from '@/components/editor/Editor'
 import React from 'react'
 
 const Document = () => {
   return (
-    <div>Document</div>
+    <div>
+      <Editor />
+    </div>
   )
 }
 

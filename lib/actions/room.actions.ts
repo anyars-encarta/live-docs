8a826360285bea8a24aa7373ef6369bc578c5f9b
anyars-encarta <anyarsencarta@gmail.com>
2024-07-25 17:32:56 +0000

    Create a createDocument server action

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { nanoid } from 'nanoid';
+import { liveblocks } from '../liveblocks';
+import { revalidatePath } from 'next/cache';
+import { parseStringify } from '../utils';
+
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+    const roomId = nanoid();
+
+    try {
+        const metadata = {
+            creatroId: userId,
+            email,
+            title: 'Untitled'
+        }
+
+        const usersAccesses: RoomAccesses = {
+            [email]: ['room:write']
+        }
+
+        const room = await liveblocks.createRoom(roomId, {
+            metadata,
+            usersAccesses,
+            defaultAccesses: []
+        });
+
+        revalidatePath('/');
+
+        return parseStringify(room);
+    } catch (e) {
+        console.log(`Error happened while creating a room: ${e}`)
+    }
+}

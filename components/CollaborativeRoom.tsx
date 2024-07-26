c6b47b1fe413f6a8c5e43abe7477a3a838a9a39d
anyars-encarta <anyarsencarta@gmail.com>
2024-07-26 16:44:49 +0000

    Install shadcn input

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,15 +10,27 @@ import {
 } from '@clerk/nextjs';
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense';
 import ActiveCollaborators from './ActiveCollaborators';
+import { useRef, useState } from 'react';
 
 const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
+    const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+    const [editing, setEditing] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLDivElement>(null);
+
     return (
         <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className='collaborative-room'>
                     <Header>
-                        <div className='flex w-fit items-center justify-center gap-2'>
-                            <p className='document-title'>Share</p>
+                        <div ref={containerRef} className='flex w-fit items-center justify-center gap-2'>
+                            {editing && !loading ? (
+                                <Input />
+                            ):(
+
+                            )}
                         </div>
 
                         <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>

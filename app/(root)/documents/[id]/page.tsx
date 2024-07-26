76c5dccab3472fd5919fadb1e6e14801a12f5d74
anyars-encarta <anyarsencarta@gmail.com>
2024-07-26 11:02:36 +0000

    Display CollaborativeRoom for a specific document

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,11 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom';
 
 const Document = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom />
+    </main>
   )
-}
+};
 
-export default Document
+export default Document;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,7 @@ const Home = async () => {
     <main className='home-container'>
       <Header className='sticky left-0 top-0'>
         <div className='flex items-center gap-2 lg:gap-4'>
-          Notification List
+          {/* TODO Notification List */}
           <SignedIn>
             <UserButton />
           </SignedIn>

7624fa9ae75aca10ed73f5602845bdd37ea4a3f4
anyars-encarta <anyarsencarta@gmail.com>
2024-07-27 10:40:13 +0000

    Fetch and display list of documents

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -65,3 +65,13 @@ export const updateDocument = async (roomId: string, title: string) => {
         console.log(`Error happened while updating a room: ${e}`)
     }
 }
+
+export const getDocuments = async (email: string) => {
+    try {
+        const rooms = await liveblocks.getRooms({ userId: email });
+
+        return parseStringify(rooms);
+    } catch (e) {
+        console.log(`Error happened while getting rooms: ${e}`);
+    }
+}

33c99936c64c6d14354a6ea6bf784fc2399c0fce
anyars-encarta <anyarsencarta@gmail.com>
2024-07-27 16:26:36 +0000

    Add resolveMentionSuggestions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -6,16 +6,28 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerUser } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const {user: clerkUser} = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint='/api/liveblocks-auth'
       resolveUsers={async ({ userIds }) => {
-        const users = await getClerUser({ userIds });
+        const users = await getClerkUsers({ userIds });
 
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({ 
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        })
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient, EmailAddress } from "@clerk/nextjs/server"
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
     try {
@@ -21,5 +22,27 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
         return parseStringify(sortedUsers);
     } catch (e) {
         console.log(`Error fetching users: ${e}`)
+    }
+}
+
+export const getDocumentUsers = async (
+    { roomId, currentUser, text}: { roomId: string, currentUser: string, text: string}
+) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+        if (text.length) {
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+
+            return parseStringify(filteredUsers);
+        }
+
+        return parseStringify(users);
+    } catch (e) {
+        console.log(`Error fetching document users: ${e}`)
     }
 }

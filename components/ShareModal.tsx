cb546ab06b71612e26fa61c0717276eb5d38151d
anyars-encarta <anyarsencarta@gmail.com>
2024-07-28 10:38:30 +0000

    Install Shadcn Dialog

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -4,8 +4,16 @@ import { useState } from "react"
 import { Button } from "./ui/button"
 import { useSelf } from "@liveblocks/react/suspense";
 
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
-const ShareModal = ({ roomId, collaborators, creatorId, currentUserType}: ShareDocumentDialogProps) => {
+const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: ShareDocumentDialogProps) => {
     const user = useSelf();
 
     const [open, setOpen] = useState(false);
@@ -13,11 +21,22 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType}: ShareD
     const [email, setEmail] = useState('');
     const [userType, setUserType] = useState<UserType>('viewer');
 
-    const shareDocumentHandler = async () => {}
-    
-  return (
-    <Button>Share</Button>
-  )
+    const shareDocumentHandler = async () => { }
+
+    return (
+        <Dialog>
+            <DialogTrigger>Share</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default ShareModal

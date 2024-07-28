d56dac75ee37cf8a7c1bde94679c6bf3acd6cfe2
anyars-encarta <anyarsencarta@gmail.com>
2024-07-28 15:05:25 +0000

    Create deleteDocument action

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,7 @@ import { nanoid } from 'nanoid';
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
 
 export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid();
@@ -115,5 +116,15 @@ export const removeCollaborator = async ({ roomId, email }: { roomId: string, em
         return parseStringify(updatedRoom);
     } catch (e) {
         console.log(`Error happened while removing a collaborator: ${e}`)
+    }
+}
+
+export const deleteDocument = async (roomId: string) => {
+    try {
+        await liveblocks.deleteRoom(roomId)
+        revalidatePath('/')
+        redirect('/')
+    } catch (e) {
+        console.log(`Error deleting a room: ${e}`)
     }
 }

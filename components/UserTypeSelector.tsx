619c15414d5c5eb56621d35e794fd2b6f16939f1
anyars-encarta <anyarsencarta@gmail.com>
2024-07-28 11:14:22 +0000

    Install shadcn select component

--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
-const UserTypeSelector = ({userType, setUserType, onClickHandler}: UserTypeSelectorParams) => {
-  return (
-    <div>UserTypeSelector</div>
-  )
+const UserTypeSelector = ({ userType, setUserType, onClickHandler }: UserTypeSelectorParams) => {
+    return (
+        <div>
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="light">Light</SelectItem>
+                    <SelectItem value="dark">Dark</SelectItem>
+                    <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+            </Select>
+        </div>
+    )
 }
 
 export default UserTypeSelector
